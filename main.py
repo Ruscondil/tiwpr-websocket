d84276df298b7fd0c4a91fd438f468251755547f
Kacper Świderski <kacpers2001@o2.pl>
2024-05-30 19:45:30 +0200

    roomView update

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ async def handle_message(websocket, path):
                 if room_name not in rooms:
                     rooms[room_name] = [websocket]
                     players_no_room.remove(websocket)  # Remove player from players_no_room list
-                    await websocket.send(json.dumps({'message': f'Room {room_name} created and joined'}))
+                    await websocket.send(json.dumps({'message': 'Joined room info', 'room_name': room_name, 'players': 1}))
                 else:
-                    await websocket.send(json.dumps({'message': f'Room {room_name} already exists'}))
+                    await websocket.send(json.dumps({'message': f'Room {room_name} already exists'})) #TODO
                 room_info = {room: len(players) for room, players in rooms.items()}
                 for player in players_no_room:
                     await player.send(json.dumps({'message': 'Room info', 'rooms': room_info}))
@@ -39,7 +39,7 @@ async def handle_message(websocket, path):
                     players_no_room.remove(websocket)
                     if len(rooms[room_name]) == 2:  # If the room now has two players
                         await rooms[room_name][0].send(json.dumps({'action': 'start_game'}))  # Send start_game action to the host
-                        await rooms[room_name][1].send(json.dumps({'message': 'Waiting for start of the game'}))  # Send message to the other player
+                        await rooms[room_name][1].send(json.dumps({'message': 'Joined room info', 'room_name': room_name, 'players': 2}))
                 else:
                     await websocket.send(json.dumps({'message': 'Room is full or does not exist'}))
     finally:
